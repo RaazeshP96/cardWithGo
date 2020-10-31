994be4422aab7f5e312992bf099fb5d5fb360844
Raazeshp96 <raazeshp96@gmail.com>
2020-10-31 12:58:33 +0545

    for loop in slice

--- main.go
+++ main.go
@@ -8,7 +8,9 @@ func main() {
 	// var card string = "this is card"
 	cards := []string{"Five of diamond", newCard()}
 	cards = append(cards, "Nine of Sphare")
-	fmt.Println(cards)
+	for index, card := range cards {
+		fmt.Println(index, card)
+	}
 }
 func newCard() string {
 	return "Ace of spread"
