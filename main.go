24d4224aec0abf345d977637f34e9fbbec55dff2
Raazeshp96 <raazeshp96@gmail.com>
2020-10-31 12:56:28 +0545

    apend in slice

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 func main() {
 	// var card string = "this is card"
 	cards := []string{"Five of diamond", newCard()}
+	cards = append(cards, "Nine of Sphare")
 	fmt.Println(cards)
 }
 func newCard() string {
