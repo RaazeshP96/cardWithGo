4af027e08dcc0363a8a070a588ee6059d0094ffd
Raazeshp96 <raazeshp96@gmail.com>
2020-11-01 13:23:45 +0545

    adding shuffle function

--- deck.go
+++ deck.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
+	"os"
 	"strings"
+	"time"
 )
 
 // create a new type of "deck"
@@ -16,6 +19,7 @@ func (d deck) print() {
 		fmt.Println(index, card)
 	}
 }
+
 func newDeck() deck {
 	cards := deck{}
 	cardSuits := []string{"Sphare", "Diamonds", "Hearts", "Cube"}
@@ -27,12 +31,35 @@ func newDeck() deck {
 	}
 	return cards
 }
+
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
 }
+
 func (d deck) toString() string {
 	return strings.Join([]string(d), ",")
 }
+
 func (d deck) saveToFile(filename string) error {
 	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+	for index := range d {
+		newPosition := r.Intn(len(d) - 1)
+		d[index], d[newPosition] = d[newPosition], d[index]
+	}
 }
--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 func main() {
 	cards := newDeck()
-	// hand, reamainCards := deal(cards, 5)
-	// hand.print()
-	// reamainCards.print()
-	cards.saveToFile("Mycard")
+	cards.print()
+
 }
