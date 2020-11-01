1f16b3ac8badd6a9e24f557d9a98a28dfcec32d3
Raazeshp96 <raazeshp96@gmail.com>
2020-11-01 22:58:46 +0545

    new deck test

---
+++ deck_test.go
@@ -0,0 +1,17 @@
+package main
+
+import "testing"
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+	if len(d) != 12 {
+		t.Errorf("Expected deck length of 12, but got %v", len(d))
+
+	}
+	if d[len(d)-1] != "Cubeofthree" {
+		t.Errorf("Expected  last card be cube of three, but got %v", d[len(d)-1])
+	}
+	if d[0] != "Sphareoface" {
+		t.Errorf("Expected  first card be sphare of ace, but got %v", d[0])
+	}
+}
