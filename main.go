cc8e82c73627db671b912f500f30ac24f3ba28cb
Raazeshp96 <raazeshp96@gmail.com>
2020-11-01 10:16:18 +0545

    using strings package

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
-	hand, reamainCards := deal(cards, 5)
-	hand.print()
-	reamainCards.print()
+	// hand, reamainCards := deal(cards, 5)
+	// hand.print()
+	// reamainCards.print()
+	fmt.Println(cards.toString())
 }
