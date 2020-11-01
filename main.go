9a361631a015e38c5c28ab92b0d0697b4bd450fc
Raazeshp96 <raazeshp96@gmail.com>
2020-11-01 10:23:50 +0545

    using io/ioutil package to write file

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := newDeck()
 	// hand, reamainCards := deal(cards, 5)
 	// hand.print()
 	// reamainCards.print()
-	fmt.Println(cards.toString())
+	cards.saveToFile("Mycard")
 }
